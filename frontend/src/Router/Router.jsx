68fb1b50c30df473ee2da4a445941a7f20a3e25f
raul-ra <raulradilloarevalo@gmail.com>
2023-10-12 13:00:57 +0200

    feat(aboutus):upload AboutUs page

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home"
 import Contact from "../pages/Contact"
 import DetailedCard from "../pages/DetailedCard"
 import AboutUs from "../pages/AboutUs"
+import Login from "../pages/Login"
 
 const router = createBrowserRouter([
     {

0e0825a6135c6587b6146a51cecea820eaa0d2be
Cynthia <cyn2993@gmail.com>
2023-10-17 16:03:22 +0200

    feat(admins-crud): adding admins crud in the backend

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import bcryptjs from 'bcryptjs'
 import session from 'express-session'
 import connection from './database/db.js';
 import userRoutes from './router/userRoutes.js';
+import adminRoutes from './router/adminRoutes.js';
 
 const app = express()
 app.disable('x-powered-by')
@@ -16,6 +17,7 @@ app.use(session({
     saveUninitialized: true
 }))
 
+app.use("/admins", adminRoutes)
 app.use("/users", userRoutes)
 
 const PORT = process.env.PORT ?? 3000 //default port 3000
--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -11,6 +11,13 @@ insert into users(email, user_password) values
 ('cyntia@gmail.com', '1234'),
 ('wanda@gmail.com', '1234ss');
 SELECT *, BIN_TO_UUID(id) id FROM users;
+create table admins(
+    id binary(16) primary key default (uuid_to_bin(uuid())),
+    email varchar(50) not null,
+    admin_password varchar(60) not null,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 SHOW GRANTS FOR 'root'@'localhost';
 GRANT ALL PRIVILEGES ON madxtreme.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
@@ -18,12 +25,14 @@ CREATE TABLE activities (
     id_activity BINARY(16) primary key DEFAULT (UUID_TO_BIN(UUID())),
     title VARCHAR(50) NOT NULL,
     user_id BINARY(16),
+    admin_id BINARY(16),
     act_description VARCHAR(60) NOT NULL,
     price INT NOT NULL,
     stock INT NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (admin_id) REFERENCES admins(id)
 );
 SELECT * FROM activities;
 insert into activities(title, act_description, price, stock) values

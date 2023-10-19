fd513e29c5aec5dbb4a38444407f39fd73c41170
andsainz <dreasvi@gmail.com>
2023-10-19 20:07:34 +0200

    feat(footer): adjusting banner position

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,20 +4,21 @@ import instagram from "../../assets/icons/instagram.png";
 import facebook from "../../assets/icons/facebook.png";
 import tiktok from "../../assets/icons/tiktok.png";
 import twitter from "../../assets/icons/twitter.png";
+import './Footer.css'
 
 function Footer() {
     return (
-        <main className="text-center w-100" color="black" bgColor="white" aria-label="Footer">
+        <main>
             <section
-                style={{ backgroundColor: "#D10505", height: 90 }}
-                className="text-white d-flex justify-content-center align-items-center">
-                <h3>Tus deportes de aventura en Madrid</h3>
+                aria-label="Footer"
+                className="banner text-white d-flex justify-content-center align-items-center">
+                <h3 className="slogan-txt">Tus deportes de aventura en Madrid</h3>
             </section>
+            <div className="icon-container">
             <section width="100%">
                 <img
                     aria-label="logotype"
                     className="mt-4"
-                    width="15%"
                     src={logotype}
                     alt="Image of logotype"
                 />
@@ -28,6 +29,7 @@ function Footer() {
                 <img src={twitter} aria-label="twitter-icon" alt="Twitter icon" />
                 <img src={tiktok} aria-label="tiktok-icon" alt="Tik tok icon" />
             </section>
+            </div>
             <div className="text-center p-3">
                 © 2023 Copyright:
                 <a className="text-black" href="https://madXtreme.com/">

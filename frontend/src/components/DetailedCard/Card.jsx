c04eec8187cc40161aa289fac14ff5a34098028b
andsainz <dreasvi@gmail.com>
2023-10-26 12:08:10 +0200

    feat(stock): adding stock stats in climbing page

--- frontend/src/components/DetailedCard/Card.jsx
+++ frontend/src/components/DetailedCard/Card.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
-function Cards({ activity_image, bookingDate , price, stock}) {
+
+function Cards({ activity_image, bookingDate , price, stock, onReserve}) {
   return (
     <main className="main mt-4">
       <Card className="section" aria-label="Card">
@@ -9,9 +10,9 @@ function Cards({ activity_image, bookingDate , price, stock}) {
           <h5><Card.Text>{bookingDate}</Card.Text></h5>
           <section className="d-flex">
             <Card.Text className='text-price'>{price} €</Card.Text>
-            <button className="detail-button">
-              Reservar actividad
-            </button>
+            <button className="detail-button" onClick={onReserve}>
+            Reservar actividad
+          </button>
           </section>
           <Card.Text>Plazas disponibles: {stock}</Card.Text>
         </Card.Body>
@@ -24,5 +25,6 @@ Cards.propTypes = {
   bookingDate: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   stock: PropTypes.number.isRequired,
+  onReserve: PropTypes.func.isRequired, 
 };
 export default Cards;
--- frontend/src/pages/Climbing.jsx
+++ frontend/src/pages/Climbing.jsx
@@ -9,37 +9,65 @@ import climbing3 from '../assets/activitiesImages/climbing3.png'
 
 function Climbing() {
   const [activities, setActivities] = useState([]);
+  const [availableStock, setAvailableStock] = useState(0);
+  const [showResetButton, setShowResetButton] = useState(false);
   const cardIndexToShow = 0;
+
   useEffect(() => {
-    activitiesService
-      .getProducts()
-      .then((fetchedActivities) => {
+    const fetchActivities = async () => {
+      try {
+        const fetchedActivities = await activitiesService.getProducts();
         setActivities(fetchedActivities);
-      })
-      .catch((error) => {
+
+        const reservedStock = localStorage.getItem('reservedStock') || 0;
+        const initialStock = fetchedActivities.length > 0 ? fetchedActivities[0].stock : 0;
+        setAvailableStock(initialStock - parseInt(reservedStock, 10));
+      } catch (error) {
         console.error('Error fetching activities', error);
-      });
+      }
+    };
+
+    fetchActivities();
   }, []);
+
+  const handleReserve = () => {
+    if (availableStock >= 1) {
+      setAvailableStock(availableStock - 1);
+
+      const reservedStock = localStorage.getItem('reservedStock') || 0;
+      localStorage.setItem('reservedStock', parseInt(reservedStock, 10) + 1);
+    } else {
+      alert('No hay plazas disponibles');
+    }
+  };
+  const handleReset = () => {
+    const initialStock = activities.length > 0 ? activities[0].stock : 0;
+    setAvailableStock(initialStock);
+    localStorage.removeItem('reservedStock');
+  };
+
   return (
     <>
       <Images
-      image1={climbing1}
-      image2={climbing2}
-      image3={climbing3} />
-       <Description 
-       title={"Escalada deportiva"}
-      description={"¡Vive la emoción de la escalada con cuerdas en un entorno natural único en la Sierra de Madrid! Nuestra experiencia de escalada con cuerdas te ofrece!"} 
-      text1={"Ubicación Escénica: Disfruta de las majestuosas montañas y vistas panorámicas."}
-      text2={"Aventura en la Roca: Tanto para principiantes como para expertos, ¡hay rutas para todos!"}
-      text3={"Seguridad en Todo Momento: Equipamiento y guías profesionales para una experiencia segura."}
-      duration={"Duración 6 horas  |  Idioma: Español/Inglés"} 
-      opinion={"🌟🌟🌟🌟🌟"} />
+        image1={climbing1}
+        image2={climbing2}
+        image3={climbing3} />
+      <Description
+        title={"Escalada deportiva"}
+        description={"¡Vive la emoción de la escalada con cuerdas en un entorno natural único en la Sierra de Madrid! Nuestra experiencia de escalada con cuerdas te ofrece!"}
+        text1={"Ubicación Escénica: Disfruta de las majestuosas montañas y vistas panorámicas."}
+        text2={"Aventura en la Roca: Tanto para principiantes como para expertos, ¡hay rutas para todos!"}
+        text3={"Seguridad en Todo Momento: Equipamiento y guías profesionales para una experiencia segura."}
+        duration={"Duración 6 horas  |  Idioma: Español/Inglés"}
+        opinion={"🌟🌟🌟🌟🌟"} />
+      {showResetButton && <button onClick={handleReset}>Resetear Contador</button>}
       {cardIndexToShow < activities.length && (
         <Card
           activity_image={activities[cardIndexToShow].activity_image}
           bookingDate={activities[cardIndexToShow].bookingDate}
           price={activities[cardIndexToShow].price}
-          stock={activities[cardIndexToShow].stock}
+          stock={availableStock}
+          onReserve={handleReserve}
         />
       )}
     </>

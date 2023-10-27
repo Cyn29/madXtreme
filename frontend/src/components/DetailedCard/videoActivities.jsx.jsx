2959403741a5000da36a6e085b0cbe758e9050c9
Jaime Martínez <jimi_jr_9@hotmail.com>
2023-10-27 03:33:04 +0200

    feat(youtube video):  adding video in activities page

--- frontend/src/components/DetailedCard/videoActivities.jsx.jsx
+++ frontend/src/components/DetailedCard/videoActivities.jsx.jsx
@@ -1,21 +1,18 @@
-
 import { Row, Col } from 'react-bootstrap';
-
 function YouTubeVideo() {
   return (
     <header>
       <Row>
         <Col md={12}>
           <video
-            autoPlay 
+            autoPlay
             width="100%"
             height="auto"
           >
             <source
-              src="https://res.cloudinary.com/dqj4pvyva/video/upload/v1698277665/video_2160p_ogi7ia.mp4" 
+              src="https://res.cloudinary.com/dqj4pvyva/video/upload/v1698366596/activityVideo_ki9j1v.mp4"
               type="video/mp4"
             />
-
           </video>
         </Col>
       </Row>
